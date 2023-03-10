47f119d995c42db3ec74737e52f7b06b327203d9
sbourgeois02 <90526583+sbourgeois02@users.noreply.github.com>
2023-03-09 21:17:09 -0800

    Get users from discord
    8 manhours spent trying to do this, keeps only showing myself and the bot. Needs fixing but dont want to lose the little progress I have

--- main.py
+++ main.py
@@ -5,9 +5,12 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 import mysqlConnect as dbConn
+
+import pprint
 
 #load the environment variables
 load_dotenv()
@@ -15,15 +18,37 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 #connect to discord
-client = discord.Client(intents=discord.Intents.default())
+intents=discord.Intents.default()
+intents.members = True
+intents.presences = True
+client = discord.Client(intents=intents)
+
+client=commands.Bot(command_prefix = '$', intents=intents)
+
 
 #connect to the database through mysqlConnect.py
-dbConn.main()
+#dbConn.main()
 
+
+
+def init_load():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+
+          print(
+            f'{client.user} is connected to the following guild:\n'
+          f'{guild.name}\n'
+          )
+
+    members = '\n - '.join([f"{member.name} ({member.status})" for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 @client.event
 async def on_ready():
+   
     print(f'{client.user} has connected to Discord!')
+   
+    init_load()
     
 
 client.run(TOKEN)
