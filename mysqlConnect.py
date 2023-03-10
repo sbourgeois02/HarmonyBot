a886365f1e4eccdcf7bfd6b38cf5f22fff9fd3f4
sbourgeois02 <90526583+sbourgeois02@users.noreply.github.com>
2023-03-10 13:24:33 -0800

    Began creating initial mysql inserts
    some pseudocode and unchecked code, will need to checked before deployment

--- mysqlConnect.py
+++ mysqlConnect.py
@@ -24,3 +24,36 @@ def main():
     results = cursor.fetchone()
 
     print(results)
+
+#onload the bot will check the database and load into it unadded things
+def onLoad(userList):
+    connection = MySQLdb.connect(host=os.getenv('DB_HOST'),
+                                 user=os.getenv('DB_USER'),
+                                 passwd=os.getenv('DB_PASSWORD'),
+                                 db=os.getenv('DB_SCHEMA'))
+    
+    cursor = connection.cursor()
+    cursor.execute("select database();")
+    db = cursor.fetchone()
+
+    if db:
+        print("You're connected to database: ", db)
+    else:
+        print('Not connected.')
+
+    cursor.execute('Select UserName, UserTag from user')
+    usersInDB = cursor.fetch()
+
+    #pseudologic
+    #if user exists in database drop from userList
+    #once all existing users are dropped from userList they will be added to the db
+
+    insertSQL = 'Insert into user(UserName, UserTag, RoleID, NumStrikes) values (%s, %s, %i, %i, %i);'
+    
+    values = []
+    while userList > 0:
+        values.append(userList[0].Name, userList[0].tag, userList[0].status, userList[0].role, 0)
+        userList.pop(0)
+
+    cursor.executemany()
+        
