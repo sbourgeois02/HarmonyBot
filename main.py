34515481ac6c5d304b453a124d9ff98cd53993a6
sbourgeois02 <90526583+sbourgeois02@users.noreply.github.com>
2023-03-12 22:00:24 -0700

    Added Ping command and started CreateCommand

--- main.py
+++ main.py
@@ -5,8 +5,11 @@
 import os
 
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
 from dotenv import load_dotenv
+
+import re
 
 import mysqlConnect as dbConn
 
@@ -18,12 +21,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 #connect to discord
-intents=discord.Intents.default()
-intents.members = True
-intents.presences = True
+intents=discord.Intents.all()
+# intents.members = True
+# intents.presences = True
+# intents.message_content = True
 client = discord.Client(intents=intents)
 
-client=commands.Bot(intents=intents, command_prefix = '$')
+client=commands.Bot(intents=intents, command_prefix = "$")
 
 
 def init_load():
@@ -53,12 +57,29 @@ def init_load():
 async def on_ready():
    
     print(f'{client.user} has connected to Discord!')
-   
+    
     init_load()
     
+@client.command()
+async def ping(ctx):
+    await ctx.channel.send("pong")
 
+@client.command(name="CreateCommand")
+async def CreateCommand(ctx, *args):
+    print(*args)
+    splitArgs = re.split("\+\+\+", *args)
+    print(splitArgs)
+
+    newCommandName = splitArgs[0]
+    newCommandPermission = splitArgs[1]
+    newCommandScript = splitArgs[2]
+
+    print("New Command\nName: ", newCommandName, "\nPermission Level: ", newCommandPermission,
+           "\nScript: ", newCommandScript)
+    
+    dbConn.storeCommands(newCommandName, newCommandPermission, newCommandScript)
+
+    await ctx.channel.send("Command Created")
 client.run(TOKEN)
-
-
 
 
--- mysqlConnect.py
+++ mysqlConnect.py
@@ -81,19 +81,33 @@ def onLoad(userList, roleList):
 
     print(addUserList)
 
+    # #check if roles are in database
+    # cursor.execute('Select RoleID from role')
+    # rolesInDB = cursor.fetchall()
 
+    # print("Stored Roles: ", rolesInDB)
+    # print(rolesInDB[0][0:1])
+    # for role in roleList:
+    #     print(str(role[0])[0:6])
+
+    # for role in roleList:
+    #     for currDBRole in rolesInDB:
+    #         if str(role[0])[0:6] is currDBRole[1:7]:
+    #             roleList.pop(0)
+
+    # print(roleList)
 
     #add roles
     insertRolesSQL = 'Insert into role(RoleID, RoleName) values (%s, %s)'
 
     roleValues = []
-    print(roleList)
+    #print(roleList)
     for role in roleList:
-        print(role)
+        #print(role)
         roleValues.append((str(role[0])[0:6], role[1]))
         roleList.pop(0)
 
-    print(roleValues)
+    #print(roleValues)
 
     cursor.executemany(insertRolesSQL, roleValues)
 
@@ -108,7 +122,7 @@ def onLoad(userList, roleList):
         userValues.append((addUserList[0][0], addUserList[0][1], addUserList[0][2], str(1062091327392731247)[0:6]))
         addUserList.pop(0)
 
-    print(userValues)
+    #print(userValues)
 
     cursor.executemany(insertUsersSQL, userValues)
 
@@ -123,4 +137,25 @@ def onLoad(userList, roleList):
     #close database
     connection.close()
 
-        
+def storeCommands(name, perm, script):
+    connection = MySQLdb.connect(host=os.getenv('DB_HOST'),user=os.getenv('DB_USER'),passwd=os.getenv('DB_PASSWORD'),db=os.getenv('DB_SCHEMA'))
+
+    cursor = connection.cursor()
+    cursor.execute("select database();")
+    db = cursor.fetchone()
+
+    if db:
+        print("You're connected to database: ", db)
+    else:
+        print('Not connected.')
+
+
+    sqlInput = "Insert into commands(CommandInput, CommandAction, CommandMinRoleID) values(%s, %s, %s)"
+    commandValues = [name, script, perm]
+
+    cursor.execute(sqlInput, commandValues)
+
+    # Commit your changes in the database
+    connection.commit()
+    #close database
+    connection.close()
