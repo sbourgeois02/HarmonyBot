9ed28a34dda8d734bb0e4d42a81e20acc7386619
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2023-04-23 13:56:56 -0700

    added sidebar and changed CSS file name

--- PHP/index.php
+++ PHP/index.php
@@ -6,87 +6,96 @@
 <html lang="en">
 <head>
     <title>HarmonyBot WebGUI</title>
-    <link rel="stylesheet" href="includes/tableStyles.css">
+    <link rel="stylesheet" href="includes/styles.css">
 </head>
 
 <body>
-    <div>
-        <h1>HarmonyBot WebGUI</h1>
-        <h2>User Data Table</h2>
-        <table>
-            <tr>
-                <th>UserName</th>
-                <th>UserTag</th>
-                <th>Status</th>
-                <th>Role</th>
-            </tr>
-            <?php
-                $sql = "SELECT * FROM user;";
-                $result = mysqli_query($conn, $sql);
-                $resultCheck = mysqli_num_rows($result);
-                if ($resultCheck > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        // get the Role Name from the Role ID
-                        // insert into variable $name
-                        $roleID = $row["UserRoleID"];
-                        $roleSQL = "SELECT RoleName FROM role WHERE RoleID = $roleID;";
-                        $roleResult = mysqli_query($conn, $roleSQL);
-                        $roleName = mysqli_fetch_assoc($roleResult);
-                        $name = $roleName["RoleName"];
-                        mysqli_free_result($roleResult);
-                        // get the Status Name from the Status ID
-                        // insert into variable $status
-                        $statusID = $row["UserStatusID"];
-                        $statusSQL = "SELECT StatusName FROM status WHERE StatusID = $statusID;";
-                        $statusResult = mysqli_query($conn, $statusSQL);
-                        $statusName = mysqli_fetch_assoc($statusResult);
-                        $status = $statusName["StatusName"];
-                        mysqli_free_result($statusResult);
-                        // display the user table
-                        echo "<tr><td>" . $row["UserName"] . "</td><td>#" . $row["UserTag"] . "</td><td>" . $status . "</td><td>" . $name . "</td></tr>";
-                    }
-                }
-            ?>
-        </table>
-    </div>
-    <p></p>
-    <div>
-        <form method="post" action="process.php">
-            <label for="user">Select a User: </label>
-            <select name="user" id="user">
+    <div class="main">
+        <div class="sidebar">
+            <a href="index.php">Home</a>
+            <a href="user.php">User</a>
+            <a href="role.php">Role</a>
+            <a href="status.php">Status</a>
+        </div>
+        <div class="content">
+            <h1>HarmonyBot WebGUI</h1>
+            <h2>User Data Table</h2>
+            <table>
+                <tr>
+                    <th>UserName</th>
+                    <th>UserTag</th>
+                    <th>Status</th>
+                    <th>Role</th>
+                </tr>
                 <?php
-                    $userName = mysqli_query($conn, "SELECT UserName FROM user;");
-                    while ($users = mysqli_fetch_assoc($userName)):;
+                    $sql = "SELECT * FROM user;";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if ($resultCheck > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            // get the Role Name from the Role ID
+                            // insert into variable $name
+                            $roleID = $row["UserRoleID"];
+                            $roleSQL = "SELECT RoleName FROM role WHERE RoleID = $roleID;";
+                            $roleResult = mysqli_query($conn, $roleSQL);
+                            $roleName = mysqli_fetch_assoc($roleResult);
+                            $name = $roleName["RoleName"];
+                            mysqli_free_result($roleResult);
+                            // get the Status Name from the Status ID
+                            // insert into variable $status
+                            $statusID = $row["UserStatusID"];
+                            $statusSQL = "SELECT StatusName FROM status WHERE StatusID = $statusID;";
+                            $statusResult = mysqli_query($conn, $statusSQL);
+                            $statusName = mysqli_fetch_assoc($statusResult);
+                            $status = $statusName["StatusName"];
+                            mysqli_free_result($statusResult);
+                            // display the user table
+                            echo "<tr><td>" . $row["UserName"] . "</td><td>#" . $row["UserTag"] . "</td><td>" . $status . "</td><td>" . $name . "</td></tr>";
+                        }
+                    }
                 ?>
-                    <option value="<?php echo $users["UserName"];
-                    ?>">
-                        <?php echo $users["UserName"];
+            </table>
+        </div>
+        <p></p>
+        <div class="content">
+            <form method="post" action="process.php">
+                <label for="user">Select a User: </label>
+                <select name="user" id="user">
+                    <?php
+                        $userName = mysqli_query($conn, "SELECT UserName FROM user;");
+                        while ($users = mysqli_fetch_assoc($userName)):;
+                    ?>
+                        <option value="<?php echo $users["UserName"];
+                        ?>">
+                            <?php echo $users["UserName"];
+                            ?>
+                        </option>
+                        <?php
+                            endwhile;
                         ?>
-                    </option>
+                </select>
+                <br>
+                <label for="role">Select a Role: </label>
+                <select name="role" id="role">
+                    <?php
+                        $roleChosen = array();
+                        $roleName = mysqli_query($conn,"SELECT RoleName FROM role;");
+                        while ($roles = mysqli_fetch_assoc($roleName)):;
+                    ?>
+                        <option value="<?php echo $roles["RoleName"];
+                        ?>">
+                            <?php echo $roles["RoleName"];
+
+                            ?>
+                        </option>
                     <?php
                         endwhile;
                     ?>
-            </select>
-            <label for="role">Select a Role: </label>
-            <select name="role" id="role">
-                <?php
-                    $roleChosen = array();
-                    $roleName = mysqli_query($conn,"SELECT RoleName FROM role;");
-                    while ($roles = mysqli_fetch_assoc($roleName)):;
-                ?>
-                    <option value="<?php echo $roles["RoleName"];
-                    ?>">
-                        <?php echo $roles["RoleName"];
-
-                        ?>
-                    </option>
-                <?php
-                    endwhile;
-                ?>
-            </select>
+                </select>
+                <br>
+                <button type="submit">Submit</button>
             <br>
-            <button type="submit">Submit</button>
-        <br>
+        </div>
     </div>
 </body>
 </html>
