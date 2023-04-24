82b64b8d014ac737819289c9b76d3aa45ce6ba6c
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2023-04-23 20:07:51 -0700

    relocated process files and renamed

---
+++ PHP/process/roleUpdate.php
@@ -0,0 +1,23 @@
+<?php
+include_once '../../PHP/includes/dbh-inc.php';
+$user = $_POST['user'];
+$roleString = $_POST['role'];
+
+// get the Role ID
+$roleSQL = "SELECT RoleID FROM role WHERE RoleName = '$roleString';";
+$roleResult = mysqli_query($conn, $roleSQL);
+$roleID = mysqli_fetch_assoc($roleResult);
+$id = $roleID["RoleID"];
+mysqli_free_result($roleResult);
+
+$updateUserSQL = "UPDATE user SET UserRoleID = $id WHERE UserName = '$user'";
+$result = mysqli_query($conn, $updateUserSQL);
+if ($result) {
+    echo $user . "'s role updated to: " . $roleString;
+} else {
+    echo "Error: " . $insertUser . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+header("Location: ../index.php?update=success");
+?>
