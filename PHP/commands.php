f66ef2a3e385b51697fa82d7dfb1058f1dd45538
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2023-04-23 20:32:39 -0700

    display Command table and read RoleID into RoleName from database

--- PHP/commands.php
+++ PHP/commands.php
@@ -1,3 +1,7 @@
+<?php
+    include_once 'includes/dbh-inc.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,4 +22,37 @@
             <button onclick="window.location.href='modwords.php'">Modwords</button>
         </div>
     </div>
+    <div class="main">
+        <div class="content">
+            <h1>Commands Data Table</h1>
+            <table>
+                <tr>
+                    <th>CommandID</th>
+                    <th>CommandInput</th>
+                    <th>CommandAction</th>
+                    <th>CommandOutput</th>
+                    <th>CommandMinRoleID</th>
+                </tr>
+                <?php
+                    $sql = "SELECT * FROM commands;";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if ($resultCheck > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            // get the RoleName from role table to display instead of RoleID
+                            $roleID = $row['CommandMinRoleID'];
+                            $roleSQL = "SELECT RoleName FROM role WHERE RoleID = $roleID;";
+                            $roleResult = mysqli_query($conn, $roleSQL);
+                            $roleName = mysqli_fetch_assoc($roleResult);
+                            $rName = $roleName['RoleName'];
+                            mysqli_free_result($roleResult);
+                            // display the data
+                            echo "<tr><td>" . $row['CommandID'] . "</td><td>" . $row['CommandInput'] . "</td><td>" . $row['CommandAction'] . "</td><td>" . $row['CommandOutput'] . "</td><td>" . $rName . "</td></tr>";
+                        }
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
 </body>
+</html>
