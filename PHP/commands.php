f8acc85bbe303ff794163cdaf39ac00d3b71aa1b
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2023-04-23 20:19:07 -0700

    added pages with same navbar

---
+++ PHP/commands.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Commands</title>
+    <link rel="stylesheet" href="includes/styles.css">
+</head>
+
+<!-- This is the same navbar on all pages -->
+<body>
+    <div class="banner">
+        <div class="title">
+            <button onclick="window.open('https://github.com/sbourgeois02/HarmonyBot','_blank')">HarmonyBot WebGUI</button>
+        </div>
+        <div class="buttons">
+            <button onclick="window.location.href='index.php'">Users</button>
+            <button onclick="window.location.href='commands.php'">Commands</button>
+            <button onclick="window.location.href='roles.php'">Roles</button>
+            <button onclick="window.location.href='modwords.php'">Modwords</button>
+        </div>
+    </div>
+</body>
