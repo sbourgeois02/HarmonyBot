bd93e73077966c0d5b7ca1fcef6aad40273656f2
sbourgeois02 <90526583+sbourgeois02@users.noreply.github.com>
2023-04-25 23:04:09 -0700

    Update Role.1.0.sql

--- res/Role.1.0.sql
+++ res/Role.1.0.sql
@@ -5,3 +5,5 @@ INSERT INTO HarmonyBot.Role(RoleID,RoleName) VALUES(1, "SuperUser");
 INSERT INTO HarmonyBot.Role(RoleID,RoleName) VALUES(2, "Moderator");
 INSERT INTO HarmonyBot.Role(RoleID,RoleName) VALUES(3, "PowerMod");
 INSERT INTO HarmonyBot.Role(RoleID,RoleName) VALUES(4, "Administrator");
+
+INSERT INTO HarmonyBot.Status(StatusID, StatusName) VALUES(0, "Offline")
